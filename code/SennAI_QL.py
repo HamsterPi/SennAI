41093f8931747e06584cbb110763eb44b26b77fc
Patrick Gildea <patrick.gildea3@mail.dcu.ie>
2020-03-03 14:52:36 +0000

    Updated SennAI_QL.py to add load_and_simulate() function to load state of previously saved AIs

--- code/SennAI_QL.py
+++ code/SennAI_QL.py
@@ -1,9 +1,7 @@
 import sys
+import numpy as np
 import math
 import random
-import numpy as np
-import matplotlib.pyplot as plt
-
 import gym
 import race_data
 
@@ -15,16 +13,14 @@ def simulate():
     total_reward_list = []
     training_done = False
     gen_threshold = 1000
-    env.set_view(True)
+    env.set_view(False)
     for gen in range(NUM_GENS):
 
         total_reward_list.append(total_reward)
-        if gen == 50000:
-            plt.plot(total_reward_list)
-            plt.ylabel('rewards')
-            plt.show()
-            env.save_memory('50000')
-            break
+
+        if gen == 10000: #to generation 10000
+            env.save_memory('10000_aft.npy') #saves q-table at 10000th generation
+            env.set_view(True)
 
         obv = env.reset()
         start_state = state_to_bucket(obv)
@@ -54,6 +50,60 @@ def simulate():
         # Update parameters
         explore_rate = get_explore_rate(gen)
         learning_rate = get_learning_rate(gen)
+
+
+
+def load_and_simulate():
+    print("Start loading history")
+    history_list = ['5000_aft.npy']
+
+    # load data from history file
+    print("Start updating q_table")
+    discount_factor = 0.99
+    i = 0
+    for list in history_list:
+        history = load_data(list)
+        learning_rate = get_learning_rate(0)
+        print(list)
+        file_size = len(history)
+        print("file size : " + str(file_size))
+        for data in history:
+            start_state, action, reward, state, done = data
+            env.recall(start_state, action, reward, state, done)
+            top_q = np.amax(q_table[state])
+            q_table[start_state + (action,)] += learning_rate * (reward + discount_factor * (top_q) - q_table[start_state + (action,)])
+            if done == True:
+                i += 1
+                learning_rate = get_learning_rate(i)
+
+    print("Updating q_table is complete")
+
+
+    # simulate
+    env.set_view(True)
+    for gen in range(NUM_GENS):
+        obv = env.reset()
+        start_state = state_to_bucket(obv)
+        total_reward = 0
+
+        for t in range(MAX_T):
+            action = select_action(start_state, 0.01)
+            obv, reward, done, _ = env.step(action)
+            state = state_to_bucket(obv)
+            env.recall(start_state, action, reward, state, done)
+            start_state = state
+            total_reward += reward
+            top_q = np.amax(q_table[state])
+            q_table[start_state + (action,)] += learning_rate * (reward + discount_factor * (top_q) - q_table[start_state + (action,)])
+            env.render()
+            if done or t >= MAX_T - 1:
+                print("Generation %d finished after %i time steps with total reward = %f."
+                      % (gen, t, total_reward))
+                break
+
+        learning_rate = get_learning_rate(i + gen)
+
+
 
 def select_action(state, explore_rate):
     if random.random() < explore_rate:
@@ -85,8 +135,15 @@ def state_to_bucket(state):
         bucket_indice.append(bucket_index)
     return tuple(bucket_indice)
 
-if __name__ == "__main__":
+def load_data(file):
+    np_load_old = np.load
+    np.load = lambda *a,**k: np_load_old(*a, allow_pickle=True, **k)
+    data = np.load(file)
+    np.load = np_load_old
+    return data
 
+
+if __name__ == "__main__":
     env = gym.make("SennAI-v0")
     NUM_BUCKETS = tuple((env.obs_space.high + np.ones(env.obs_space.shape)).astype(int))
     NUM_ACTIONS = env.act_space.n
@@ -101,4 +158,5 @@ if __name__ == "__main__":
     #MAX_T = np.prod(NUM_BUCKETS, dtype=int) * 100
 
     q_table = np.zeros(NUM_BUCKETS + (NUM_ACTIONS,), dtype=float)
-    simulate()
+    #simulate()
+    load_and_simulate()
